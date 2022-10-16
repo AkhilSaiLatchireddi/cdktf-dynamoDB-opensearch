2745d55843adb2feb07695da03c453c8675cd82d
AkhilSaiLatchireddi <na>
2022-10-16 10:43:59 +0000

    added security best practices

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 from constructs import Construct
 from cdktf import App, TerraformStack
-# from imports.aws.provider import AwsProvider
-# from imports.aws.dynamodb import dynamodb
-from cdktf_cdktf_provider_aws.dynamodb_table import DynamodbTable
+from cdktf_cdktf_provider_aws.dynamodb_table import DynamodbTable,DynamodbTableServerSideEncryption
 from cdktf_cdktf_provider_aws.dynamodb_table_item import DynamodbTableItem
 from cdktf_cdktf_provider_aws.provider import AwsProvider
-from cdktf_cdktf_provider_aws.opensearch_domain import OpensearchDomain,OpensearchDomainClusterConfig,OpensearchDomainClusterConfigZoneAwarenessConfig,OpensearchDomainEbsOptions
+from cdktf_cdktf_provider_aws.opensearch_domain import OpensearchDomain,OpensearchDomainNodeToNodeEncryption,\
+OpensearchDomainClusterConfig,OpensearchDomainClusterConfigZoneAwarenessConfig,OpensearchDomainEbsOptions,OpensearchDomainEncryptAtRest,\
+OpensearchDomainAdvancedSecurityOptions,OpensearchDomainAdvancedSecurityOptionsMasterUserOptions
+from cdktf_cdktf_provider_aws.sns_topic import SnsTopic
+from cdktf_cdktf_provider_aws.sns_topic_subscription import SnsTopicSubscription
+from cdktf_cdktf_provider_aws.kms_key import KmsKey
+from cdktf_cdktf_provider_aws.cloudwatch_metric_alarm import CloudwatchMetricAlarm
+from cdktf_cdktf_provider_aws.cloudwatch_log_metric_filter import CloudwatchLogMetricFilter
+from cdktf_cdktf_provider_aws.data_aws_iam_role import DataAwsIamRole
 import json
 
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
-
-        # define resources here
-        AwsProvider(self, 'Aws', region='us-east-1')
+        region='us-east-1'
+        AwsProvider(self, 'Aws', region=region)
+        tags={
+                'Project':'Oslash'
+            }
         
+        ############ Dyanmo DB Table ###############
+        ddbkmskey=KmsKey(self, 'KMSforDyanmoDB',description="KMS key to Encrypt the DyanmoDB")
         table=DynamodbTable(
             self, 
             'Oslashtable',
@@ -23,34 +33,41 @@ class MyStack(TerraformStack):
             read_capacity=1,
             write_capacity=1,
             hash_key='Id',
-            # range_key='Date',
+            server_side_encryption=DynamodbTableServerSideEncryption(
+                enabled=True,
+                kms_key_arn=ddbkmskey.arn),
+            replica= [{
+                'regionName' : "us-east-2",
+                'kms_key_arn' : ddbkmskey.arn
+                
+            }],
             attribute= [
               { 'name' :'Id', 'type' : 'S' },
-            #   { 'name' :'Date', 'type' : 'N' },
             ],
-            
+            tags=tags
             )
             
+            
         itemv1= {
-  "Id": {"S": "dsfjhdkhljsdh"},
-  "Date": {"N": "11111"},
-  "two": {"N": "22222"},
-  "three": {"N": "33333"},
-  "four": {"N": "44444"}
-}
+                  "Id": {"S": "dsfjhdkhljsdh"},
+                  "Date": {"N": "11111"},
+                  "two": {"N": "22222"},
+                  "three": {"N": "33333"},
+                  "four": {"N": "44444"}
+                }
 
         DynamodbTableItem(
             self, 
             'Oslashtableitem',
             hash_key='Id',
             table_name=table.name,
-            # range_key='Date',
             item = json.dumps(itemv1),
             )
             
             
-            ############################opensearch##########
-            
+        ###########################opensearch##########
+        OpenSearchkmskey=KmsKey(self, 'KMSforOpenSearch',description="KMS key to Encrypt the Open Search Domain")   
+        OpenSearchMasterRole=DataAwsIamRole(self, 'MsterIAMUserforOS',name='AWSServiceRoleForAmazonOpenSearchService')
         OpensearchDomain(
             self, 
             'OpenseachDomain',
@@ -67,13 +84,41 @@ class MyStack(TerraformStack):
                 zone_awareness_config= OpensearchDomainClusterConfigZoneAwarenessConfig(
                     availability_zone_count=2,
                     )
+            ),
+            tags=tags,
+            node_to_node_encryption=OpensearchDomainNodeToNodeEncryption(enabled=True),
+            encrypt_at_rest=OpensearchDomainEncryptAtRest(enabled=True,kms_key_id=OpenSearchkmskey.key_id),
+            engine_version="1.3",
+            
+            advanced_security_options=OpensearchDomainAdvancedSecurityOptions(
+                enabled=True,
+                anonymous_auth_enabled=True,
+                internal_user_database_enabled=False,
+                master_user_options=OpensearchDomainAdvancedSecurityOptionsMasterUserOptions(master_user_arn=OpenSearchMasterRole.arn))
             )
             
-            
-            )
-            
+        ####################### Monitoring #############
         
-
+        kmskey=KmsKey(self, 'KMSforSNS',description="KMS key to Encrypt the SNS Topic")
+        snstopic=SnsTopic(self, 'OpenseachAlertTopic', display_name='OpenseachAlertTopic-oslash',name='OpenseachAlertTopic-oslash',kms_master_key_id=kmskey.key_id,tags=tags)
+        SnsTopicSubscription(self, 'OpenseachAlertTopicSubcription',endpoint='emailid@emaildomain.com',protocol='email',topic_arn=snstopic.arn)
+        
+        # CloudwatchLogMetricFilter(self, 'opensearchMetricfilter', name='opensearchMetricfilter', metric_transformation=,pattern=)
+        CloudwatchMetricAlarm(self, 'opensearchMetricfilter',alarm_name="opensearchMetricfilter",
+        comparison_operator='GreaterThanOrEqualToThreshold',
+        evaluation_periods=1,
+        alarm_actions=[snstopic.arn],
+        actions_enabled = True,
+        alarm_description="will send an alert if threshold crosses 70",
+        namespace='AWS/ES',
+        metric_name='CPUUtilization',
+        statistic="Average",
+        period=60,
+        threshold=70,
+        )
+        
+        
+            
 
 app = App()
 MyStack(app, "cdktf-oslash")
